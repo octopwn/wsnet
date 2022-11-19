d3726c91b339a9df90593112df058fa6609bde21
SkelSec <info@skelsecprojects.com>
2022-11-19 12:22:56 +0100

    adding ssl

--- wsnet/_version.py
+++ wsnet/_version.py
@@ -1,5 +1,5 @@
 
-__version__ = "0.0.10"
+__version__ = "0.0.11"
 __banner__ = \
 """
 # wsnet %s 
--- wsnet/server/wsserver.py
+++ wsnet/server/wsserver.py
@@ -1,6 +1,7 @@
 import traceback
 import websockets
 import asyncio
+import ssl
 
 from wsnet import logger
 from wsnet.agent.agent import WSNETAgent
@@ -30,7 +31,17 @@ class WSNETWSServer:
 
 async def amain(args):
 	try:
-		server = WSNETWSServer(listen_ip=args.ip, listen_port=args.port)
+		ssl_ctx = None
+		if args.ssl_cert is not None:
+			ssl_ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+			if args.ssl_key is None:
+				raise Exception('TLS certificate is set but no keyfile!')
+			ssl_ctx.load_cert_chain(args.ssl_cert, args.ssl_key)
+			if args.ssl_ca is not None:
+				ssl_ctx.load_verify_locations(args.ssl_ca)
+				ssl_ctx.verify_mode = ssl.CERT_REQUIRED
+
+		server = WSNETWSServer(listen_ip=args.ip, listen_port=args.port, ssl_ctx=ssl_ctx)
 		await server.run()
 
 	except:
@@ -43,6 +54,9 @@ def main():
 	parser.add_argument('--ip', default='127.0.0.1', help='Listen IP')
 	parser.add_argument('--port', type=int, default=8700, help='Listen port')
 	parser.add_argument('-v', '--verbose', action='count', default=0, help='Increase verbosity, can be stacked')
+	parser.add_argument('--ssl-cert', help='Certificate file for SSL')
+	parser.add_argument('--ssl-key',  help='Key file for SSL')
+	parser.add_argument('--ssl-ca',  help='CA cert file for client cert validations')
 
 	args = parser.parse_args()
 	if args.verbose == 1:
