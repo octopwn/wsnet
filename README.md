c27ad47e55a3e2399cbaf801f19f5b694965f69f
Skelsec <tamas.jos@skelsecprojects.com>
2023-11-25 12:33:57 +0100

    added security features

--- README.md
+++ README.md
@@ -3,11 +3,16 @@ A lightweight protocol implementation to perform TCP and authentication proxying
 It's actually Layer3 agnostic, but websockets was easy to use.
 
 # If you are here for the OctoPwn
+
 ## Install
 Either use PIP `pip install wsnet` or clone + install via GitHub.
+
 ## Starting server
 After installing you should have a new binary called `wsnet-wssserver`.  
-It has a help menu, but really there is not much to change there.
+By default the server generates its own certificate and private key because Firefox doesn't allow mixed content on localhost, but it is allowed in Chrome. This behaviour can be disabled by using the `--plaintext` switch.  
+By default this server generates a random path which is used to protect the server agains potential malicious JS from other pages trying to use the server. This can be disabled by `--disable-security`, but this also disables the `Origin` header check which is part of the same protection mechanism.  
+
+If you wish to run it on localhost without any security (it's your life) use the following: `wsnet-wssserver --plaintext --disable-security`
 
 # If you are here to see what this is
 Sorry, for the time being you'll need to read the code.
--- wsnet/server/wsserver.py
+++ wsnet/server/wsserver.py
@@ -2,18 +2,80 @@ import traceback
 import websockets
 import asyncio
 import ssl
+import os
 import uuid
 from typing import Dict
 from urllib.parse import urlparse
 
 from wsnet import logger
 from wsnet.agent.agent import WSNETAgent
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, NoEncryption, PublicFormat
+from datetime import datetime, timedelta
 
 WSNET_ALLOWED_ORIGINS = {
 	'0.0.0.0' : 1,
 	'127.0.0.1' : 1,
 	'localhost' : 1,
 }
+
+
+def create_ca(ca_cert_path: str, ca_key_path: str):
+    ca_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
+    ca_subject = x509.Name([
+        x509.NameAttribute(NameOID.COMMON_NAME, u"My Local CA"),
+    ])
+    ca_cert = (
+        x509.CertificateBuilder()
+        .subject_name(ca_subject)
+        .issuer_name(ca_subject)
+        .public_key(ca_key.public_key())
+        .serial_number(x509.random_serial_number())
+        .not_valid_before(datetime.utcnow())
+        .not_valid_after(datetime.utcnow() + timedelta(days=3650))
+        .add_extension(x509.BasicConstraints(ca=True, path_length=None), critical=True)
+        .sign(ca_key, hashes.SHA256())
+    )
+    
+    with open(ca_cert_path, "wb") as f:
+        f.write(ca_cert.public_bytes(Encoding.PEM))
+    with open(ca_key_path, "wb") as f:
+        f.write(ca_key.private_bytes(Encoding.PEM, PrivateFormat.TraditionalOpenSSL, NoEncryption()))
+
+    return ca_cert, ca_key
+
+
+def create_server_cert(ca_cert, ca_key, server_cert_path: str, server_key_path: str):
+    server_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
+    server_subject = x509.Name([
+        x509.NameAttribute(NameOID.COMMON_NAME, u"localhost"),
+    ])
+    server_cert = (
+        x509.CertificateBuilder()
+        .subject_name(server_subject)
+        .issuer_name(ca_cert.subject)
+        .public_key(server_key.public_key())
+        .serial_number(x509.random_serial_number())
+        .not_valid_before(datetime.utcnow())
+        .not_valid_after(datetime.utcnow() + timedelta(days=365))
+        .add_extension(x509.SubjectAlternativeName([x509.DNSName(u"localhost")]), critical=False)
+        .sign(ca_key, hashes.SHA256())
+    )
+    
+    with open(server_cert_path, "wb") as f:
+        f.write(server_cert.public_bytes(Encoding.PEM))
+        f.write(ca_cert.public_bytes(Encoding.PEM))
+    with open(server_key_path, "wb") as f:
+        f.write(server_key.private_bytes(Encoding.PEM, PrivateFormat.TraditionalOpenSSL, NoEncryption()))
 
 class WSNETWSServer:
 	def __init__(self, listen_ip:str = '127.0.0.1', listen_port:int = 8700, ssl_ctx:ssl.SSLContext = None, secret:str = None, disable_origin_check:bool = False, allowed_origins:Dict[str, int] = WSNET_ALLOWED_ORIGINS, disable_security:bool = False):
@@ -132,7 +194,7 @@ def main():
 	parser.add_argument('--secret',  type=str, help='Secret string to protect this proxy from malicious connections')
 	parser.add_argument('--noorigin', action='store_true', help='Disables origin header validation')
 	parser.add_argument('--disable-security', action='store_true', help='Disables all security validations')
-
+	parser.add_argument('--plaintext', action='store_true', help='Disables TLS')
 
 	args = parser.parse_args()
 	if args.verbose == 1:
@@ -141,6 +203,39 @@ def main():
 	elif args.verbose > 1:
 		logging.basicConfig(level=1)
 		logger.setLevel(logging.DEBUG)
+	
+	if args.plaintext is True:
+		args.ssl_cert = None
+		args.ssl_key = None
+		args.ssl_ca = None
+	else:
+		if args.ssl_cert is None and args.ssl_key is None:
+			#check if cert is already generated
+			ca_cert_name = 'octopwn_ca_cert.pem'
+			ca_key_name = 'octopwn_ca_key.pem'
+			cert_file_name = 'octopwn_ceritficate.pem'
+			key_file_name = 'octopwn_key.pem'
+			if os.path.exists(cert_file_name) and os.path.exists(key_file_name):
+				print('Using existing certificate and key files')
+				args.ssl_cert = cert_file_name
+				args.ssl_key = key_file_name
+				args.ssl_ca = None
+			else:
+				if os.path.exists(ca_cert_name) and os.path.exists(ca_key_name):
+					print('Using existing CA certificate and key files')
+					ca_cert = x509.load_pem_x509_certificate(open(ca_cert_name, 'rb').read())
+					ca_key = rsa.load_pem_private_key(open(ca_key_name, 'rb').read(), password=None)
+				else:
+					print('Generating self-signed CA')
+					ca_cert, ca_key = create_ca(ca_cert_name, ca_key_name)
+
+				print('Generating self-signed certificate and key files')
+				create_server_cert(ca_cert, ca_key, cert_file_name, key_file_name)
+				args.ssl_cert = cert_file_name
+				args.ssl_key = key_file_name
+				args.ssl_ca = None
+		elif args.ssl_cert is None or args.ssl_key is None:
+			raise Exception('You need to specify both certificate and key files!')
 
 	asyncio.run(amain(args))
 
